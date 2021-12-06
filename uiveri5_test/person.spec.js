395b431a140e055d95d4760ac398cb97fe3395c4
Herberth Alencar <herberth.alencar@sap.com>
2021-12-06 10:21:16 -0300

    new git commit test

--- uiveri5_test/person.spec.js
+++ uiveri5_test/person.spec.js
@@ -1,5 +1,15 @@
-describe('abap-person', function () {
-    it('should load the app',function() {
-      expect(true).toBeTruthy();
-    });
+describe("person", function () {
+
+    it("Should validade the home screen", function () {
+    // add actions and assertions here
+
+    expect(true).toBeTruthy();
+  });
+
+  it("should see the Home page", function () {
+    // call the page object's actions and assertions:
+    // When.onTheHomePage.iDoSomething();
+    // Then.onTheHomePage.iAssertSomething();
+  });
+
   });
